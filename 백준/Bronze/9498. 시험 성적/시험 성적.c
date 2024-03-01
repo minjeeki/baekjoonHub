514cd5d6c9c7ce6fc0c9d2855ef3ad8f8ecc05a5
minjee kim <148981647+minjeeki@users.noreply.github.com>
2024-03-01 19:08:49 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int score = 0;
+    
+    scanf("%d", &score);
+    getchar();
+    if (score >= 90)
+        printf("A");
+    else if (score >= 80)
+        printf("B");
+    else if (score >= 70)
+        printf("C");
+    else if (score >= 60)
+        printf("D");
+    else
+        printf("F");
+    return(0);
+}
