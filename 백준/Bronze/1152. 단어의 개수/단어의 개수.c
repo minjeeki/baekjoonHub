10f03dde6f1f43fb0c69a12afb7944a91581e4d6
minjee kim <148981647+minjeeki@users.noreply.github.com>
2024-03-05 22:28:01 +0900

    [Bronze II] Title: 단어의 개수, Time: 8 ms, Memory: 1968 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.c
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.c
@@ -6,14 +6,14 @@
 int main(void)
 {
     char sentence[MAX_SENTENCE];
-    int word_cnt = 0, was_char = 0, len_str = 0, idx = 0;
+    int word_cnt = 0, was_char = 0, len_str = 0;
 
     // 공백이 있는 문자열은 scanf로는 받을 수 없고, fgets()를 통해 받아야 한다.
     fgets(sentence, MAX_SENTENCE, stdin);
     //fgets()로 문장을 받으면 개행 문자 역시 문자열에 포함되기에 개행문자를 널문자로 바꿔줘야 이후 계산에서 추가되지 않는다.
     len_str = strlen(sentence);
     sentence[len_str - 1] = 0;
-    while (sentence[idx] != 0)
+    for (int idx=0; idx<(len_str - 1); idx++)
     {
         if (was_char == 0 && sentence[idx] != ' ')
         {
@@ -22,7 +22,6 @@ int main(void)
         }
         else if (was_char == 1 && sentence[idx] == ' ')
             was_char = 0;
-        idx++;
     }
     printf("%d", word_cnt);
     return 0;
