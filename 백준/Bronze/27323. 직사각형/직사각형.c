dcf1875331f993b4fe64f5a648cbd82ab9fe5f10
minjee kim <148981647+minjeeki@users.noreply.github.com>
2024-03-10 11:06:59 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 0, b = 0;
+    
+    scanf("%d", &a);
+    getchar();
+    scanf("%d", &b);
+    getchar();
+    printf("%d", a*b);
+    return 0;
+}
