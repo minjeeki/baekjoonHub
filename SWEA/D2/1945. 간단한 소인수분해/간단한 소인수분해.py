7a9881f29c45e1df55404d670485afc020277afc
minjee kim <148981647+minjeeki@users.noreply.github.com>
2024-07-29 16:59:26 +0900

    [D2] Title: 간단한 소인수분해, Time: 103 ms, Memory: 43,760 KB -BaekjoonHub

---
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
@@ -0,0 +1,12 @@
+T = int(input())
+
+for tc in range(1, T + 1):
+    num = int(input())
+    n_list = [2, 3, 5, 7, 11]
+    result = [0, 0, 0, 0, 0]
+    for idx in range(len(n_list)):
+        while num % n_list[idx] == 0:
+            result[idx] += 1
+            num /= n_list[idx]
+        result[idx] = str(result[idx])
+    print(f'#{tc} {" ".join(result)}')
